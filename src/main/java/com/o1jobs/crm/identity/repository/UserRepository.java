92b04bdb1eeed0abfd6f0eb09419a4cfc2294053
michalswig <mike@data.pl>
2026-06-04 21:23:22 +0200

    feat: add UserRepository

---
+++ src/main/java/com/o1jobs/crm/identity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.o1jobs.crm.identity.repository;
+
+import com.o1jobs.crm.identity.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUsername(String username);
+}
